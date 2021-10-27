f397a921547f0d6f26521680db259132812d1ce9
hamdi mechergui <mecherguihamdi45@gmail.com>
2021-10-27 14:57:59 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+}
